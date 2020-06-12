14ea5c271897203a48728e0ad9e504ce733f19cf
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-06-12 18:48:15 +0200

    Fixed setUsername

--- src/checkProfile.js
+++ src/checkProfile.js
@@ -1,9 +1,15 @@
-import holochain from './holochain'
+import holochain from "./holochain";
 
 export default async () => {
-    const res = await holochain.isUserCreated()
-    
-    if (!res) {
-        document.getElementById('get-username').parentNode.style.display = 'block'
-    }
-}
+  const res = await holochain.isUserCreated();
+
+  const username = document.getElementById("get-username");
+
+  if (!res) {
+    username.parentNode.style.display = "block";
+  }
+
+  username.addEventListener("username-set", (e) => {
+    username.parentNode.style.display = "none";
+  });
+};
--- src/index.js
+++ src/index.js
@@ -20,76 +20,11 @@ import { ProfilesModule } from "holochain-profiles";
 import { ApolloClientModule } from "@uprtcl/graphql";
 import checkProfile from './checkProfile';
 
-var board = null;
-var game = new Chess();
-
 checkProfile()
 manageSendInvite();
 manageInvitations();
 assignMenuEventListeners();
 
-function onDragStart(source, piece, position, orientation) {
-  // do not pick up pieces if the game is over
-  if (game.game_over()) return false;
-  // only pick up pieces for the side to move
-  if (
-    (game.turn() === "w" && piece.search(/^b/) !== -1) ||
-    (game.turn() === "b" && piece.search(/^w/) !== -1)
-  ) {
-    return false;
-  }
-}
-
-function onDrop(source, target) {
-  // see if the move is legal
-  const move = game.move({
-    from: source,
-    to: target,
-    promotion: "q", // NOTE: always promote to a queen for example simplicity
-  });
-  // illegal move
-  if (move === null) return "snapback";
-  updateStatus();
-}
-
-// update the board position after the piece snap
-// for castling, en passant, pawn promotion
-function onSnapEnd() {
-  board.position(game.fen());
-}
-
-function updateStatus() {
-  var status = "";
-  var moveColor = "White";
-  if (game.turn() === "b") {
-    moveColor = "Black";
-  }
-  // checkmate?
-  if (game.in_checkmate()) {
-    status = "Game over, " + moveColor + " is in checkmate.";
-  } else if (game.in_draw()) {
-    // draw?
-    status = "Game over, drawn position";
-  } else {
-    // game still on
-    status = moveColor + " to move";
-    // check?
-    if (game.in_check()) {
-      status += ", " + moveColor + " is in check";
-    }
-  }
-}
-
-var config = {
-  draggable: true,
-  position: "start",
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onSnapEnd: onSnapEnd,
-};
-// board = window.Chessboard('my-board', config)
-
-// updateStatus()
 
 const orchestrator = new MicroOrchestrator();
 
@@ -106,7 +41,6 @@ async function loadModules() {
     new ChessModule("chess"),
   ]);
 
-  //customElements.define('hc-chess-main', ChessMain)
 }
 
 loadModules();
